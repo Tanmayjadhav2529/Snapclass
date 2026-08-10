b6ce60ea5f11ff6a38c0e5edcd1443f7160f8206
Tanmay Jadhav <tanmayjadhav2529@gmail.com>
2026-08-10 12:31:58 +0530

    Add app and source files

---
+++ src/screens/student_screen.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+def student_screen():
+    st.title("Student Portal")
+    st.write("Student Portal - Coming Soon")
---
+++ src/screens/teacher_screen.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+def teacher_screen():
+    st.title("Teacher Portal")
+    st.write("Teacher Portal - Coming Soon")
